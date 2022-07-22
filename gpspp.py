483503c3aa539264af311f0f2b19f648cadcdb93
Andrew Tedstone <andrew.tedstone@unifr.ch>
2022-07-22 17:18:52 +0200

    add framework of new post-processing functions

---
+++ gpspp.py
@@ -0,0 +1,36 @@
+def calculate_local_origin():
+    pass
+
+
+def calculate_local_neu():
+    pass
+
+
+def apply_pole_corrections():
+    pass
+    
+
+def apply_exclusions():
+    pass
+
+
+def filter_positions():
+    pass    
+
+
+def calculate_track_displacements():
+    pass
+
+
+def smooth_track_displacements():
+    pass
+
+
+def calculate_daily_velocities():
+    pass
+
+
+def calculate_short_velocities(
+    window : int
+    )
+    pass
