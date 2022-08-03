5efb1f92cfebc982e3be26cdcf68841e211e5ecd
Andrew Tedstone <andrew.tedstone@unifr.ch>
2022-08-03 09:26:45 +0200

    adjust function refs for updated files

--- calculate_local_origin.py
+++ calculate_local_origin.py
@@ -10,24 +10,17 @@ import argparse
 import math
 import numpy as np
 
-import gps
+import gpspp
 
 p = argparse.ArgumentParser('Calculate and save origin coordinates of local cartesian grid.')
 p.add_argument('site', type=str, help='Name of site')
 p.add_argument('geod_file', type=str, help='Filepath/name of GEOD parquet file from which to take origin coordinates')
 args = p.parse_args()
 
-# Load a concatenated GEOD file.
-# Don't need much ...
-# Let's assume we're working with Parquet only.
-# Could load with Dask actualy
-
-#geod = dd.read_parquet(args.geod_file)
 geod = pd.read_parquet(args.geod_file)
 
 # We have to recalculate local cartesian for pre_res first 100 points as these are not saved in pre_res.
-pp = gps.PostProcess()
-pre_res_xyz = pp.ell2xyz(geod['Latitude'].iloc[0:100] * (math.pi/180),
+pre_res_xyz = gpspp.ell2xyz(geod['Latitude'].iloc[0:100] * (math.pi/180),
                            geod['Longitude'].iloc[0:100] * (math.pi/180),
                            geod['Height'].iloc[0:100]) 
 
